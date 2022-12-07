4d024395bc1be238515a3e42089af258a115b8b5
chumardo <dachigiorgadze98@gmail.com>
2022-12-07 19:16:37 +0400

    creating header component

---
+++ src/components/TheHeader.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="page-header">
+    <h1>{{ title }}</h1>
+    <span
+      ><img :src="require(`@/assets/img/${filename}`)" alt="About Icon"
+    /></span>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ["title", "filename"],
+};
+</script>
+
+<style scoped>
+.page-header {
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+  border-bottom-style: dashed;
+  font-size: 20px;
+  border-bottom-width: thin;
+  border-bottom-color: rgba(255, 255, 255, 0.5);
+}
+
+img {
+  width: 40px;
+  padding-bottom: 1%;
+}
+
+h1 {
+  color: rgb(23, 122, 212);
+  padding-bottom: 1%;
+}
+</style>
--- src/pages/AboutmePage.vue
+++ src/pages/AboutmePage.vue
@@ -1,10 +1,6 @@
 <template>
   <div class="container">
-    <div class="page-header">
-      <h1>About Me</h1>
-      <span><img src="@/assets/img/about.png" alt="About Icon" /></span>
-    </div>
-
+    <TheHeader title="About Me" filename="about.png"> </TheHeader>
     <div class="info">
       <div class="about">
         <h2>Web Developer</h2>
@@ -36,35 +32,24 @@
   </div>
 </template>
 
+<script>
+import TheHeader from "../components/TheHeader.vue";
+export default {
+  components: {
+    TheHeader,
+  },
+};
+</script>
+
 <style scoped>
 @import url("https://fonts.googleapis.com/css2?family=Roboto:wght@700&display=swap");
+
 .container {
   background-color: rgba(44, 45, 47, 255);
   width: 100%;
   padding-left: 17%;
   padding-right: 17%;
   padding-top: 2%;
-}
-
-.page-header {
-  display: flex;
-  flex-direction: row;
-  justify-content: space-between;
-  align-items: center;
-  border-bottom-style: dashed;
-  font-size: 20px;
-  border-bottom-width: thin;
-  border-bottom-color: rgba(255, 255, 255, 0.5);
-}
-
-img {
-  width: 40px;
-  padding-bottom: 1%;
-}
-
-h1 {
-  color: rgb(23, 122, 212);
-  padding-bottom: 1%;
 }
 
 .info {

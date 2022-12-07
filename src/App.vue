d2a285cf5e1f70caffed00f5067048545eaf0af6
chumardo <dachigiorgadze98@gmail.com>
2022-12-07 12:48:03 +0400

    animating page transitions

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,11 @@
 <template>
   <div class="main">
     <SideBar />
-    <router-view></router-view>
+    <router-view v-slot="{ Component }">
+      <transition name="route" mode="out-in">
+        <component :is="Component"></component>
+      </transition>
+    </router-view>
   </div>
 </template>
 
@@ -25,5 +29,21 @@ export default {
 .main {
   display: flex;
   justify-content: flex-start;
+}
+
+.route-enter-from {
+  transform: scale(0.5, 0.5);
+}
+
+.route-enter-active {
+  transition: all 0.3s ease-out;
+}
+
+.route-leave-to {
+  transform: scale(0.5, 0.5);
+}
+
+.route-leave-active {
+  transition: all 0.3s ease-in;
 }
 </style>

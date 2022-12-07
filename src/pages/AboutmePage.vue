68bd7cb903bdd0251c613a753294f0facb31ed39
chumardo <dachigiorgadze98@gmail.com>
2022-12-07 18:17:07 +0400

    adding and styling personal info div

--- src/pages/AboutmePage.vue
+++ src/pages/AboutmePage.vue
@@ -8,7 +8,7 @@
     <div class="info">
       <div class="about">
         <h2>Web Developer</h2>
-        <p>
+        <p class="about-info">
           Dolor reprehenderit aut repudiandae maiores recusandae. Consequatur
           dolor nisi labore commodi ipsa? Excepturi velit laboriosam at
           voluptatibus cupiditate est vel. Dolorem praesentium itaque fugiat
@@ -23,6 +23,14 @@
       </div>
       <div class="personal-info">
         <h2>Personal Information</h2>
+        <div class="fields">
+          <p class="field">Name : <span>Dachi Giorgadze</span></p>
+          <p class="field">Age : <span>24 Years</span></p>
+          <p class="field">Residance : <span>Rustavi, Georgia</span></p>
+          <p class="field">Address : <span>Modebadze Street 22/24</span></p>
+          <p class="field">Email : <span>dachi.giorgadze98@gmail.com</span></p>
+          <p class="field">Phone : <span>(+995) 593 476 776</span></p>
+        </div>
       </div>
     </div>
   </div>
@@ -61,18 +69,47 @@ h1 {
 
 .info {
   display: grid;
-  grid-template-columns: 2fr 1fr;
+  grid-template-columns: 1.9fr 1.1fr;
   color: rgb(255, 255, 255);
   padding-top: 5%;
 }
 
 .info p {
-  font-size: 14px;
   color: #e1e1e1;
   font-family: "Roboto", sans-serif;
+}
+
+.info h2 {
+  padding-bottom: 3rem;
 }
 
 .about {
   width: 90%;
 }
+
+.personal-info .fields .field {
+  color: rgb(255, 255, 255);
+  font-weight: 700;
+}
+
+.fields {
+  display: flex;
+  flex-direction: column;
+  justify-content: space-around;
+  text-align: left;
+}
+
+.fields p {
+  margin-bottom: 5%;
+}
+
+.field span {
+  color: rgb(225, 225, 225);
+  font-weight: 400;
+}
+
+/* .fields,
+.about-info {
+  padding-top: 3rem;
+} */
 </style>

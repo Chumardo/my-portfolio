ce13b00bb60eb75664d1661b3f5fb292487c76f8
chumardo <dachigiorgadze98@gmail.com>
2022-12-16 21:55:20 +0400

    creating resume content divs

--- src/pages/ResumePage.vue
+++ src/pages/ResumePage.vue
@@ -1,6 +1,14 @@
 <template>
   <TheContainer>
     <TheHeader title="My Resume" filename="resume.png" />
+    <div class="exp-edu">
+      <div class="experience">
+        <h3>EXPERIENCE</h3>
+      </div>
+      <div class="education">
+        <h3>EDUCATION</h3>
+      </div>
+    </div>
   </TheContainer>
 </template>
 
@@ -15,3 +23,37 @@ export default {
   },
 };
 </script>
+
+<style scoped>
+.exp-edu {
+  display: grid;
+  grid-template-columns: 1fr 1fr;
+  padding-top: 5%;
+}
+
+.experience,
+.education {
+  text-align: center;
+}
+
+.exp-edu h3 {
+  display: inline-block;
+  color: #e1e1e1;
+  font-family: "Roboto", sans-serif;
+  font-size: 22px;
+  letter-spacing: 0.5px;
+  border-left: 3px solid;
+  border-bottom: 3px solid;
+  border-right: 3px solid;
+  border-image: linear-gradient(
+    to top,
+    rgb(23, 122, 212),
+    25%,
+    rgba(0, 0, 0, 0) 25%
+  );
+  border-image-slice: 1;
+  padding-left: 5px;
+  padding-right: 5px;
+  padding-bottom: 3px;
+}
+</style>

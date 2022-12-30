71219480748fd2949bc854928b7bf5de52c48c3f
chumardo <dachigiorgadze98@gmail.com>
2022-12-30 13:25:18 +0400

    contact page title

--- src/pages/ContactPage.vue
+++ src/pages/ContactPage.vue
@@ -1,6 +1,11 @@
 <template>
   <TheContainer>
     <TheHeader title="Contact Me" filename="contact.png" />
+    <div class="contact">
+      <div class="talk">
+        <h3>LET'S TALK</h3>
+      </div>
+    </div>
   </TheContainer>
 </template>
 
@@ -15,3 +20,31 @@ export default {
   },
 };
 </script>
+
+<style scoped>
+.talk h3 {
+  display: inline-block;
+  color: #e1e1e1;
+  font-family: "Roboto", sans-serif;
+  font-size: 22px;
+  letter-spacing: 0.5px;
+  border-left: 3px solid;
+  border-bottom: 3px solid;
+  border-right: 3px solid;
+  border-image: linear-gradient(
+    to top,
+    rgb(23, 122, 212),
+    25%,
+    rgba(0, 0, 0, 0) 25%
+  );
+  border-image-slice: 1;
+  padding-left: 5px;
+  padding-right: 5px;
+  padding-bottom: 3px;
+}
+
+.contact {
+  text-align: center;
+  padding-top: 5%;
+}
+</style>

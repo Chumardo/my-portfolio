cfa589ce85febbda10d9abb2f286973ee019c5bc
chumardo <dachigiorgadze98@gmail.com>
2023-01-05 17:03:54 +0400

    blog card component

---
+++ src/components/BlogCard.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="card">
+    <router-link to="/">
+      <div class="cover">
+        <img
+          src="https://img.freepik.com/free-photo/wide-angle-shot-single-tree-growing-clouded-sky-during-sunset-surrounded-by-grass_181624-22807.jpg"
+          style="width: 30vh; height: 25vh"
+          alt="Blog Image"
+        />
+      </div>
+
+      <div class="blog-content">
+        <h4>Blog Title</h4>
+        <p>2 April 1998</p>
+      </div>
+    </router-link>
+  </div>
+</template>
--- src/pages/BlogPage.vue
+++ src/pages/BlogPage.vue
@@ -1,17 +1,36 @@
 <template>
   <TheContainer>
     <TheHeader title="My Blogs" filename="blog.png" />
+    <div class="blogs">
+      <BlogCard />
+
+      <BlogCard />
+
+      <BlogCard />
+    </div>
   </TheContainer>
 </template>
 
 <script>
 import TheContainer from "../components/TheContainer.vue";
 import TheHeader from "../components/TheHeader.vue";
+import BlogCard from "../components/BlogCard.vue";
 
 export default {
   components: {
     TheContainer,
     TheHeader,
+    BlogCard,
   },
 };
 </script>
+
+<style scoped>
+.blogs {
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+  padding-top: 5%;
+}
+</style>

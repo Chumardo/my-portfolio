08f0b4142ece57c055dd2184671f2e96f1271f8f
chumardo <dachigiorgadze98@gmail.com>
2023-01-09 16:52:03 +0400

    changing not found page content

--- src/pages/NotFound.vue
+++ src/pages/NotFound.vue
@@ -1,6 +1,31 @@
 <template>
-  <h2>
-    Page not found! Maybe view my
-    <router-link to="/portfolio">Projects</router-link>
-  </h2>
+  <div class="error">
+    <h1>404 page not found</h1>
+    <p>
+      The page you are looking for doesn't exist or an other error occured.
+      Choose another page from sidebar.
+    </p>
+  </div>
 </template>
+
+<style scoped>
+.error {
+  display: flex;
+  flex-direction: column;
+  text-align: center;
+  margin: auto;
+  width: 50%;
+}
+
+p {
+  font-size: 14px;
+  margin: auto;
+  width: 50%;
+  color: white;
+}
+
+h1 {
+  font-size: 50px;
+  padding-bottom: 15%;
+}
+</style>
